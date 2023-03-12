87ba9a8e1267d8d7ab0838b6ea43ea8e0facd7ae
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-03-11 19:32:02 -0500

    Added font requirement

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 # For all configs to work, you will need to install the following packages, in case you haven't yet:
-sudo pacman -S terminator picom feh
+sudo pacman -S terminator picom feh terminus-font
 # More packages may be added in the future!
