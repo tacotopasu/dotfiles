0be477a14c08571c9471dbacfcb516f5a9b732f0
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-03-11 19:33:00 -0500

    Added i3blocks requirement

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 # For all configs to work, you will need to install the following packages, in case you haven't yet:
-sudo pacman -S terminator picom feh terminus-font
+sudo pacman -S terminator picom feh terminus-font i3blocks
 # More packages may be added in the future!
