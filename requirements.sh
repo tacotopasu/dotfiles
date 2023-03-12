d129360c788103f95f0a6c74413b2de2d90341cb
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-03-11 19:08:32 -0500

    Added requirements.sh

---
+++ requirements.sh
@@ -0,0 +1,3 @@
+# For all configs to work, you will need to install the following packages, in case you haven't yet:
+sudo pacman -S terminator picom feh
+# More packages may be added in the future!
