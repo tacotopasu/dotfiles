5cf2c18bfad86bff9fc87db2ca994d0236130625
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-03-11 20:15:16 -0500

    Updated requirements

--- requirements.sh
+++ requirements.sh
@@ -1,3 +1,3 @@
 # For all configs to work, you will need to install the following packages, in case you haven't yet:
-sudo pacman -S terminator picom feh terminus-font i3blocks
+sudo pacman -S terminator picom feh terminus-font i3blocks pulseaudio pulsemixer
 # More packages may be added in the future!
