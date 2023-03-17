39b69fa453c806c36ca279667d64f09609b60a6b
たことぱす <79318163+tacotopasu@users.noreply.github.com>
2023-03-17 11:32:21 +0000

    Changed font

--- requirements.sh
+++ requirements.sh
@@ -1,4 +1,4 @@
 # For all configs to work, you will need to install the following packages, in case you haven't yet:
-sudo pacman -S terminator picom feh terminus-font i3blocks pulseaudio pulsemixer ncmpcpp mpd
+sudo pacman -S terminator picom feh ttf-jetbrains-mono-nerd i3blocks pulseaudio pulsemixer ncmpcpp mpd
 # Changing terminal emulator in progress! terminator won't be needed anymore.
 # More packages may be added in the future!
